3d2d333648c75225d586de2f625c9abc879581b5
pcp172020 <pablopalacios@student.purdueglobal.edu>
2023-04-11 15:46:11 -0500

    Completed Customer Class.

--- Pablo_Palacios_Unit6_IT481/Customer.cs
+++ Pablo_Palacios_Unit6_IT481/Customer.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Pablo_Palacios_Unit6_IT481
 {
-    internal class Customer
+    class Customer
     {
+
+        int NumberOfItems;
+
+        public Customer()
+        {
+            NumberOfItems = 6;
+        }
+
+        public Customer(int items)
+        {
+
+            int ClothingItems = items;
+            if (ClothingItems == 0)
+            {
+                NumberOfItems = GetRandomNumber(1, 20);
+            }
+            else
+            {
+                NumberOfItems = ClothingItems;
+            }
+        }
+
+        //Return the number of items
+        public int getNumberOfItems()
+        {
+            return NumberOfItems;
+        }
+
+        //Random number methods
+        private static readonly Random getrandom = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom)
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+
     }
 }
